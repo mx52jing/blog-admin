a679e64b9b814211b6a99b6230242682a1598212
mengxuan <3007492783@qq.com>
2020-04-20 16:08:14 +0800

    完善文章列表分页

--- src/api/request.js
+++ src/api/request.js
@@ -3,7 +3,7 @@ import axios from './axios'
 * 通用接口
 * */
 // 请求数据
-export const getData = (url, params = {}, requestOpt) => axios(requestOpt).get(url, params)
+export const getData = (url, params = {}, requestOpt) => axios(requestOpt).get(url, { params })
 // 提交数据
 export const postData = (url, data = {}, requestOpt) => axios(requestOpt).post(url, data)
 // 更新数据
